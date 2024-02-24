29b415e3c8544ca866e7177e10211a5f59aebe94
betlb <betlb@DESKTOP-ARP6TVV>
2024-02-24 15:45:49 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_Scraping_Akademi_Uygulaması.Models;
+using System.Diagnostics;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Search(string searchText)
+    {
+
+        string searchResult = searchText;
+        Debug.WriteLine(searchResult);// searchteki anahtar kelime buraya geldi.
+
+        using (var httpClient=new HttpClient())
+        {
+            Debug.WriteLine("kod burada");
+            var url = "https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q=&btnG=";// gidilmek istenen url
+            var response=await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseBody);// yanıt 
+            }
+            else
+            {
+                Debug.WriteLine($"Yanıt Başarısız Oldu{response.StatusCode}");
+            }
+        }
+            return RedirectToAction("Index");
+    }   
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Scraping_Akademi_Uygulaması.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
