f9b072478c6835158581f534429b5db41a36acfc
betulbodur <bet.bodur@gmail.com>
2024-02-28 23:23:57 +0300

    #pdf linkleri alındı+ban

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text;
 using System.Net.Http.Headers;
+using HtmlAgilityPack;
 
 public class HomeController : Controller
 {
+    List<string> pdfUrls = new List<string>();
+
     public ActionResult Index()
     {
         return View();
@@ -21,7 +24,8 @@ public class HomeController : Controller
         string searchResult = searchText;
         Debug.WriteLine(searchResult);// searchteki anahtar kelime buraya geldi.
 
-        if (searchResult.Contains(" ")){
+        if (searchResult.Contains(" "))
+        {
             for (int i = 0; i < searchResult.Length; i++)
             {
                 if (searchResult[i].Equals(' '))
@@ -33,9 +37,9 @@ public class HomeController : Controller
             }
         }
 
-        Debug.WriteLine("Anahtar Kelime: "+searchResult);
+        Debug.WriteLine("Anahtar Kelime: " + searchResult);
 
-        using (var httpClient=new HttpClient())
+        using (var httpClient = new HttpClient())
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             httpClient.DefaultRequestHeaders.AcceptCharset.Add(new StringWithQualityHeaderValue("utf-8"));//desteklenmeyen karakter sti hatasını engellemek için.çünkü bunu "ISO-8859-9" desteklemiyor.
@@ -46,17 +50,42 @@ public class HomeController : Controller
 
             Debug.WriteLine(url);
 
-            var response=await httpClient.GetAsync(url);
+            var response = await httpClient.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 var responseBody = await response.Content.ReadAsStringAsync();
                 Debug.WriteLine(responseBody);// yanıt 
+
+
+                HtmlDocument doc = new HtmlDocument(); //gelen yanıtı bir belgeye dönüştürüldü. buradan html kodlarını inceliycez.
+                doc.LoadHtml(await response.Content.ReadAsStringAsync());
+
+                var nodes = doc.DocumentNode.SelectNodes("//a[contains(@href, 'pdf')]");//pdf geçen tüm bağlantılar
+
+                foreach (var node in nodes)
+                {
+                    string pdfUrl = node.Attributes["href"].Value;
+                    Debug.WriteLine(pdfUrl);//!!!!        ÖNEMLİ !!!!!! pdfUrl veritabanına kayıt edilecek !!!!!!
+                    pdfUrls.Add(pdfUrl);
+                    
+                    var citationCountNode = node.SelectSingleNode(".//following-sibling::a[contains(@href, '/scholar?cites=')]");
+                  
+                    if (citationCountNode != null)
+                    {
+                        string citationCountText = citationCountNode.InnerText.Trim();
+                        Debug.WriteLine("Alıntı Sayısı: " + citationCountText);
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Alıntı sayısı bulunamadı.");
+                    }
+                }
             }
             else
             {
                 Debug.WriteLine($"Yanıt Başarısız Oldu{response.StatusCode}");
             }
-        }
-            return RedirectToAction("Index");
-    }   
+    }
+        return RedirectToAction("Index");
+    }
 }
